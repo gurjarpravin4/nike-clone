1d7c48be422b7582956b0e6f295d5ec0d1324451
Pravin Gurjar <gurjarpravin4@gmail.com>
2023-09-21 12:31:23 +0530

    Subscribe Done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full ${props.fullwidth ? 'w-full' : ''}
      ${props.backgroundColor ? `${props.borderColor} ${props.backgroundColor} ${props.textColor}` : 'border-coral-red bg-coral-red text-white'}`}>
       {props.label}
       {props.iconURL && <img src={props.iconURL} alt="arrow-right" className='ml-2 rounded-full w-5 h-5'/>}
